02cfa9ceec46038307535644cf8b38d23ddc85a7
swkim95 <sungwon.kim@cern.ch>
2023-06-20 01:55:05 +0900

    Ntupler added, tested but not validated yet

---
+++ analysis/TBntupler.py
@@ -0,0 +1,29 @@
+import ROOT
+import sys, os
+import argparse
+
+parser = argparse.ArgumentParser()
+parser.add_argument('--RunNumber', '-rn', action='store', type=int, required=True, help='Run number')
+parser.add_argument('--MaxEntry', '-me', action='store', type=int, default=-1, help='Maximum entry to ntuplize, use full event if -1')
+parser.add_argument('--FileEntry', '-fe', action='store', type=int, default=-1, help='Entry per splitted file, store all event in single file if -1')
+parser.add_argument('--doFast', '-f', action='store_true', default=False, help='If true, make fast mode ntupler')
+parser.add_argument('--output', '-o', action='store', type=str, default='./', help='Set output file dir path')
+args = parser.parse_args()
+
+rn = args.RunNumber
+mode = "Fast" if args.doFast else "Wave"
+
+outputPath = ""
+if args.output.endswith("/") : 
+    outputPath = args.output + f"Run_{rn}/Run_{rn}_{mode}/"
+else :
+    outputPath = args.output + f"/Run_{rn}/Run_{rn}_{mode}/"
+if not os.path.exists(outputPath) :
+    os.makedirs(outputPath)
+outFileName = f"ntuple_Run_{rn}_{mode}"
+
+ntupler = ROOT.TBntupler("/Users/swkim/DRC/2022_TB_at_CERN/data_sample", outputPath, outFileName, rn, args.MaxEntry, args.FileEntry)
+if args.doFast :
+    ntupler.ntuplizeFastmode()
+else :
+    ntupler.ntuplizeWaveform()
