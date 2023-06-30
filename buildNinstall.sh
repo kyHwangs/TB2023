1ae661f45050ffbc36431067c2137d6d07704f38
swkim95 <sungwon.kim@cern.ch>
2023-06-30 22:52:04 +0200

    Change env set to load only yaml from cvmfs and use local root

--- buildNinstall.sh
+++ buildNinstall.sh
@@ -1,8 +1,9 @@
 #! /bin/bash
 
-source envset.sh
+# source envset.sh
+source envset_test.sh
 mkdir build install
 cd build
-cmake .. -DCMAKE_INSTALL_PREFIX=../install
+cmake .. -DCMAKE_INSTALL_PREFIX=../install 
 make -j4 install
 cd ..
